02e5122d61ed3264c4ea5eeca923a3ecbcfef051
github-action-benchmark <github@users.noreply.github.com>
2020-10-15 04:07:58 +0000

    add Rust Benchmark (cargo) benchmark result for 6d3e0a1561fd72797ea50b64077e2c4281d85dd5

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1602732363509,
+  "lastUpdate": 1602734878694,
   "repoUrl": "https://github.com/echeran/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -449,6 +449,36 @@ window.BENCHMARK_DATA = {
             "name": "langid/overview",
             "value": 5654,
             "range": "± 248",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6d3e0a1561fd72797ea50b64077e2c4281d85dd5",
+          "message": "Merge pull request #17 from echeran/ci-bench-fixed-decimal\n\nTesting PR - in personal fork - enable utils/fixed-decimal benchmark",
+          "timestamp": "2020-10-14T21:04:09-07:00",
+          "tree_id": "0f8610177898fa1eed700f55e45ecb6f2e6d3a01",
+          "url": "https://github.com/echeran/icu4x/commit/6d3e0a1561fd72797ea50b64077e2c4281d85dd5"
+        },
+        "date": 1602734871640,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 5548,
+            "range": "± 66",
             "unit": "ns/iter"
           }
         ]
