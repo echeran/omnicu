b5c20408fde3d3ac6a32a5b5190526dba0a0ed71
github-action-benchmark <github@users.noreply.github.com>
2020-10-15 04:08:05 +0000

    add Rust Benchmark (cargo) benchmark result for 6d3e0a1561fd72797ea50b64077e2c4281d85dd5

---
+++ dev/components/datetime/data.js
@@ -0,0 +1,68 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1602734885569,
+  "repoUrl": "https://github.com/echeran/icu4x",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6d3e0a1561fd72797ea50b64077e2c4281d85dd5",
+          "message": "Merge pull request #17 from echeran/ci-bench-fixed-decimal\n\nTesting PR - in personal fork - enable utils/fixed-decimal benchmark",
+          "timestamp": "2020-10-14T21:04:09-07:00",
+          "tree_id": "0f8610177898fa1eed700f55e45ecb6f2e6d3a01",
+          "url": "https://github.com/echeran/icu4x/commit/6d3e0a1561fd72797ea50b64077e2c4281d85dd5"
+        },
+        "date": 1602734879678,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 1029700,
+            "range": "± 2751",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6d3e0a1561fd72797ea50b64077e2c4281d85dd5",
+          "message": "Merge pull request #17 from echeran/ci-bench-fixed-decimal\n\nTesting PR - in personal fork - enable utils/fixed-decimal benchmark",
+          "timestamp": "2020-10-14T21:04:09-07:00",
+          "tree_id": "0f8610177898fa1eed700f55e45ecb6f2e6d3a01",
+          "url": "https://github.com/echeran/icu4x/commit/6d3e0a1561fd72797ea50b64077e2c4281d85dd5"
+        },
+        "date": 1602734879678,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 1029700,
+            "range": "± 2751",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
