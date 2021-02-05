8bb0d9e91cb2aec0ed12f2b371e711bd0e75bf4c
github-action-benchmark <github@users.noreply.github.com>
2021-02-05 05:35:35 +0000

    add Rust Benchmark (cargo) benchmark result for ae89da479dfbbcfc73fbaa977cd3f4ecfd0e4fac

--- dev/components/datetime/data.js
+++ dev/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612502275322,
+  "lastUpdate": 1612503335758,
   "repoUrl": "https://github.com/echeran/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -239,6 +239,36 @@ window.BENCHMARK_DATA = {
             "name": "datetime/overview",
             "value": 1132813,
             "range": "± 66994",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ae89da479dfbbcfc73fbaa977cd3f4ecfd0e4fac",
+          "message": "Allow some clippy lints (#474)",
+          "timestamp": "2021-02-02T17:40:53-08:00",
+          "tree_id": "b24a02430c995fda5f39e5982049523f7037643c",
+          "url": "https://github.com/echeran/icu4x/commit/ae89da479dfbbcfc73fbaa977cd3f4ecfd0e4fac"
+        },
+        "date": 1612503335237,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 1055331,
+            "range": "± 30649",
             "unit": "ns/iter"
           }
         ]
