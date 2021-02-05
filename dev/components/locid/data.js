1f24e9e8c0faebc5ed81eb9b22e919ee03229906
github-action-benchmark <github@users.noreply.github.com>
2021-02-05 05:17:51 +0000

    add Rust Benchmark (cargo) benchmark result for d9f01c66ca6126c48cc65dc817e3c820cf7d7f78

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1606776728180,
+  "lastUpdate": 1612502271143,
   "repoUrl": "https://github.com/echeran/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -659,6 +659,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 9201,
             "range": "± 216",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d9f01c66ca6126c48cc65dc817e3c820cf7d7f78",
+          "message": "Merge pull request #21 from echeran/gregtatum/memory-benchmark-upload-fix\n\nTesting PR on echeran's fork for gregtatum/memory-benchmark-upload-fix",
+          "timestamp": "2021-02-04T21:12:24-08:00",
+          "tree_id": "a7c739d23fce79b570dccb6eed947f4f4351130a",
+          "url": "https://github.com/echeran/icu4x/commit/d9f01c66ca6126c48cc65dc817e3c820cf7d7f78"
+        },
+        "date": 1612502267028,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3163,
+            "range": "± 216",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5061,
+            "range": "± 308",
             "unit": "ns/iter"
           }
         ]
